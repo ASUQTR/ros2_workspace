055a8833ef886e18cf11658a7a58e20c0a4b176d
Loïc Pouget <loic.pgt@gmail.com>
2026-04-29 11:47:40 -0400

    Remove useless comments

--- env/desktop/ros_entrypoint.sh
+++ env/desktop/ros_entrypoint.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-# Base ROS
 source /opt/ros/humble/setup.bash
 
-# Underlay (toujours présent)
 source /opt/underlay_ws/install/setup.bash
 
-# Overlay (volume dev)
 if [ -f "/workspace/install/setup.bash" ]; then
     source /workspace/install/setup.bash
 fi
 
-# Perf fix BLAS (repris de ton Docker Jetson 👍)
+# Optimisations de performance pour les bibliothèques de calcul
 export OPENBLAS_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
