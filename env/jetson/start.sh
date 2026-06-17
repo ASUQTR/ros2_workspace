72b0b202d5fa2e99bc8870f0191d83a558994c28
Gabriel Tremblay <gabriel.tremblay@mailbox.org>
2026-06-16 20:53:26 -0400

    Working version june 16 2026

--- env/jetson/start.sh
+++ env/jetson/start.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 ENV_FILE="$(pwd)/env/jetson"
 
-if ! docker images --format json | grep -q "ros2-jetson"; then
+if ! docker images --format json | grep -q "asuqtr_ros2"; then
   echo -e "[\033[0;31m ERROR \033[0m] Missing docker image.."
   bash $ENV_FILE/setup.sh
 fi
 
-docker run -it --rm \
-  --group-add dialout \
-  --net=host \
-  -v $(pwd)/workspace:/workspace \
-  --name ros2-desktop \
-  ros2-jetson
+docker compose -f $ENV_FILE/docker-compose.yaml up
