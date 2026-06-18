a127e6bc54927593d493ae9536a41b9dfd880525
ÉmileLahaie <137105334+Blue1003d@users.noreply.github.com>
2026-06-18 17:29:13 -0400

    Update lqr_solver.py

--- workspace/packages/sub_control/sub_control/lqr_solver.py
+++ workspace/packages/sub_control/sub_control/lqr_solver.py
@@ -23,41 +23,45 @@ Bm = np.zeros((12, 8), dtype=np.float32)
 
 # --- Kinetics (Linear Accelerations: u_dot, v_dot, w_dot) ---
 # Surge (u_dot) affected by horizontal thrusters
-Bm[6][0] = -0.10172661870503597122302158273381
-Bm[6][1] = -0.10172661870503597122302158273381
-Bm[6][6] = 0.10172661870503597122302158273381
-Bm[6][7] = 0.10172661870503597122302158273381
-
-# Sway (v_dot)
-Bm[7][0] = 0.10172661870503597122302158273381
-Bm[7][1] = -0.10172661870503597122302158273381
-Bm[7][6] = 0.10172661870503597122302158273381
-Bm[7][7] = -0.10172661870503597122302158273381
-
-# Heave (w_dot) affected by vertical thrusters
-Bm[8][2] = -0.14388489208633093525179856115108
-Bm[8][3] = -0.14388489208633093525179856115108
-Bm[8][4] = -0.14388489208633093525179856115108
-Bm[8][5] = -0.14388489208633093525179856115108
-
-# --- Kinetics (Angular Accelerations: p_dot, q_dot, r_dot) ---
-# Roll (p_dot)
-Bm[9][2] = -1.6212566279156605511438599985064
-Bm[9][3] = 1.6212566279156605511438599985064
-Bm[9][4] = -1.6212566279156605511438599985064
-Bm[9][5] = 1.6212566279156605511438599985064
-
-# Pitch (q_dot)
-Bm[10][2] = -0.57207517706764820701578272265908
-Bm[10][3] = -0.57207517706764820701578272265908
-Bm[10][4] = 0.57207517706764820701578272265908
-Bm[10][5] = 0.57207517706764820701578272265908
-
-# Yaw (r_dot)
-Bm[11][0] = 1.3277256221063703250844264122267
-Bm[11][1] = -1.3277256221063703250844264122267
-Bm[11][6] = -1.3277256221063703250844264122267
-Bm[11][7] = 1.3277256221063703250844264122267
+  Bm[0][0:8] = 0.0
+  Bm[1][0:8] = 0.0
+  Bm[2][0:8] = 0.0
+  Bm[3][0:8] = 0.0
+  Bm[4][0:8] = 0.0
+  Bm[5][0:8] = 0.0
+  Bm[6][0] = -0.021117481435499889398735391488808
+  Bm[6][1] = -0.021117481435499889398735391488808
+  Bm[6][2:6] = 0.0
+  Bm[6][6] = 0.021117481435499889398735391488808
+  Bm[6][7] = 0.021117481435499889398735391488808
+  Bm[7][0] = 0.01902284924040905501271856144737
+  Bm[7][1] = -0.01902284924040905501271856144737
+  Bm[7][2:6] = 0.0
+  Bm[7][6] = 0.01902284924040905501271856144737
+  Bm[7][7] = -0.01902284924040905501271856144737
+  Bm[8][0:2] = 0.0
+  Bm[8][2] = -0.026906434569178295633265292004767
+  Bm[8][3] = -0.026906434569178295633265292004767
+  Bm[8][4] = -0.026906434569178295633265292004767
+  Bm[8][5] = -0.026906434569178295633265292004767
+  Bm[8][6:8] = 0.0
+  Bm[9][0:2] = 0.0
+  Bm[9][2] = -0.47145328719723183391003460207612
+  Bm[9][3] = 0.47145328719723183391003460207612
+  Bm[9][4] = -0.47145328719723183391003460207612
+  Bm[9][5] = 0.47145328719723183391003460207612
+  Bm[9][6:8] = 0.0
+  Bm[10][0:2] = 0.0
+  Bm[10][2] = -0.16128655774647620680552224586649
+  Bm[10][3] = -0.16128655774647620680552224586649
+  Bm[10][4] = 0.16128655774647620680552224586649
+  Bm[10][5] = 0.16128655774647620680552224586649
+  Bm[10][6:8] = 0.0
+  Bm[11][0] = 0.39463536905835478514559256667363
+  Bm[11][1] = -0.39463536905835478514559256667363
+  Bm[11][2:6] = 0.0
+  Bm[11][6] = -0.39463536905835478514559256667363
+  Bm[11][7] = 0.39463536905835478514559256667363
 
 
 # ==============================================================================
@@ -254,118 +258,101 @@ class SubLQRSolver:
         # y_dot, z_dot, roll_dot, pitch_dot, yaw_dot).
 
         # Row 0: x_dot (World North Velocity)
-        Am[0][0] = 0.0
-        Am[0][1] = 0.0
-        Am[0][2] = 0.0
-        Am[0][3] = v * (sin_roll * sin_yaw + cos_roll * cos_yaw * sin_pitch) + w * (cos_roll * sin_yaw - 1.0 * cos_yaw * sin_pitch * sin_roll)
-        Am[0][4] = w * cos_pitch * cos_roll * cos_yaw - 1.0 * u * cos_yaw * sin_pitch + v * cos_pitch * cos_yaw * sin_roll
-        Am[0][5] = w * (cos_yaw * sin_roll - 1.0 * cos_roll * sin_pitch * sin_yaw) - 1.0 * v * (cos_roll * cos_yaw + sin_pitch * sin_roll * sin_yaw) - 1.0 * u * cos_pitch * sin_yaw
-        Am[0][6] = cos_pitch * cos_yaw
-        Am[0][7] = cos_yaw * sin_pitch * sin_roll - 1.0 * cos_roll * sin_yaw
-        Am[0][8] = sin_roll * sin_yaw + cos_roll * cos_yaw * sin_pitch
+        Am[0][0:3] = 0.0
+        Am[0][3] = v*(sin_roll*sin_yaw + cos_roll*cos_yaw*sin_pitch) + w*(cos_roll*sin_yaw - 1.0*cos_yaw*sin_pitch*sin_roll)
+        Am[0][4] = w*cos_pitch*cos_roll*cos_yaw - 1.0*u*cos_yaw*sin_pitch + v*cos_pitch*cos_yaw*sin_roll
+        Am[0][5] = w*(cos_yaw*sin_roll - 1.0*cos_roll*sin_pitch*sin_yaw) - 1.0*v*(cos_roll*cos_yaw + sin_pitch*sin_roll*sin_yaw) - 1.0*u*cos_pitch*sin_yaw
+        Am[0][6] = cos_pitch*cos_yaw
+        Am[0][7] = cos_yaw*sin_pitch*sin_roll - 1.0*cos_roll*sin_yaw
+        Am[0][8] = sin_roll*sin_yaw + cos_roll*cos_yaw*sin_pitch
         Am[0][9:12] = 0.0
-
-        # Row 1: y_dot (World East Velocity)
         Am[1][0:3] = 0.0
-        Am[1][3] = - 1.0 * w * (cos_roll * cos_yaw + sin_pitch * sin_roll * sin_yaw) - 1.0 * v * (cos_yaw * sin_roll - 1.0 * cos_roll * sin_pitch * sin_yaw)
-        Am[1][4] = w * cos_pitch * cos_roll * sin_yaw - 1.0 * u * sin_pitch * sin_yaw + v * cos_pitch * sin_roll * sin_yaw
-        Am[1][5] = w * (sin_roll * sin_yaw + cos_roll * cos_yaw * sin_pitch) - 1.0 * v * (cos_roll * sin_yaw - 1.0 * cos_yaw * sin_pitch * sin_roll) + u * cos_pitch * cos_yaw
-        Am[1][6] = cos_pitch * sin_yaw
-        Am[1][7] = cos_roll * cos_yaw + sin_pitch * sin_roll * sin_yaw
-        Am[1][8] = cos_roll * sin_pitch * sin_yaw - 1.0 * cos_yaw * sin_roll
+        Am[1][3] = - 1.0*w*(cos_roll*cos_yaw + sin_pitch*sin_roll*sin_yaw) - 1.0*v*(cos_yaw*sin_roll - 1.0*cos_roll*sin_pitch*sin_yaw)
+        Am[1][4] = w*cos_pitch*cos_roll*sin_yaw - 1.0*u*sin_pitch*sin_yaw + v*cos_pitch*sin_roll*sin_yaw
+        Am[1][5] = w*(sin_roll*sin_yaw + cos_roll*cos_yaw*sin_pitch) - 1.0*v*(cos_roll*sin_yaw - 1.0*cos_yaw*sin_pitch*sin_roll) + u*cos_pitch*cos_yaw
+        Am[1][6] = cos_pitch*sin_yaw
+        Am[1][7] = cos_roll*cos_yaw + sin_pitch*sin_roll*sin_yaw
+        Am[1][8] = cos_roll*sin_pitch*sin_yaw - 1.0*cos_yaw*sin_roll
         Am[1][9:12] = 0.0
-
-        # Row 2: z_dot (World Down Velocity)
         Am[2][0:3] = 0.0
-        Am[2][3] = v * cos_pitch * cos_roll - 1.0 * w * cos_pitch * sin_roll
-        Am[2][4] = - 1.0 * u * cos_pitch - 1.0 * w * cos_roll * sin_pitch - 1.0 * v * sin_pitch * sin_roll
+        Am[2][3] = v*cos_pitch*cos_roll - 1.0*w*cos_pitch*sin_roll
+        Am[2][4] = - 1.0*u*cos_pitch - 1.0*w*cos_roll*sin_pitch - 1.0*v*sin_pitch*sin_roll
         Am[2][5] = 0.0
-        Am[2][6] = -1.0 * sin_pitch
-        Am[2][7] = cos_pitch * sin_roll
-        Am[2][8] = cos_pitch * cos_roll
+        Am[2][6] = -1.0*sin_pitch
+        Am[2][7] = cos_pitch*sin_roll
+        Am[2][8] = cos_pitch*cos_roll
         Am[2][9:12] = 0.0
-
-        # Row 3: roll_dot (World Roll Rate)
         Am[3][0:3] = 0.0
-        Am[3][3] = q * cos_roll * tan_pitch - 1.0 * r * tan_pitch * sin_roll
-        Am[3][4] = r * cos_roll * (tan_pitch ** 2 + 1.0) + q * sin_roll * (tan_pitch ** 2 + 1.0)
+        Am[3][3] = q*cos_roll*tan_pitch - 1.0*r*tan_pitch*sin_roll
+        Am[3][4] = r*cos_roll*(tan_pitch**2 + 1.0) + q*sin_roll*(tan_pitch**2 + 1.0)
         Am[3][5:9] = 0.0
         Am[3][9] = 1.0
-        Am[3][10] = tan_pitch * sin_roll
-        Am[3][11] = cos_roll * tan_pitch
-
-        # Row 4: pitch_dot (World Pitch Rate)
+        Am[3][10] = tan_pitch*sin_roll
+        Am[3][11] = cos_roll*tan_pitch
         Am[4][0:3] = 0.0
-        Am[4][3] = - 1.0 * r * cos_roll - 1.0 * q * sin_roll
+        Am[4][3] = - 1.0*r*cos_roll - 1.0*q*sin_roll
         Am[4][4:10] = 0.0
         Am[4][10] = cos_roll
-        Am[4][11] = -1.0 * sin_roll
-
-        # Row 5: yaw_dot (World Yaw Rate)
+        Am[4][11] = -1.0*sin_roll
         Am[5][0:3] = 0.0
-        Am[5][3] = (q * cos_roll) / cos_pitch - (1.0 * r * sin_roll) / cos_pitch
-        Am[5][4] = (r * cos_roll * sin_pitch) / cos_pitch ** 2 + (q * sin_pitch * sin_roll) / cos_pitch ** 2
+        Am[5][3] = (q*cos_roll)/cos_pitch - (1.0*r*sin_roll)/cos_pitch
+        Am[5][4] = (r*cos_roll*sin_pitch)/cos_pitch**2 + (q*sin_pitch*sin_roll)/cos_pitch**2
         Am[5][5:10] = 0.0
-        Am[5][10] = sin_roll / cos_pitch
-        Am[5][11] = cos_roll / cos_pitch
-
-        # --- KINETIC & HYDRODYNAMIC COUPLING (Rows 6-11) ---
-        # These rows calculate accelerations based on hydrodynamic drag, Coriolis 
-        # forces (cross-coupling between axes), and hydrostatic restoring forces 
-        # (gravity/buoyancy vectors shifting as the Sub pitches and rolls).
-
-        # Row 6: u_dot (Surge Acceleration)
+        Am[5][10] = sin_roll/cos_pitch
+        Am[5][11] = cos_roll/cos_pitch
         Am[6][0:4] = 0.0
-        Am[6][4] = 0.07194244604316546762589928057554 * cos_pitch * (41092.031908954495559091375453296 * radius ** 3 - 234.459)
+        Am[6][4] = 0.61533413727402152970770551635211*cos_pitch
         Am[6][5] = 0.0
-        Am[6][6] = self.damping_sign * 1.5978417266187050359712230215827 # Linear Drag (X)
-        Am[6][7] = r    # Coriolis cross-coupling
-        Am[6][8] = -1.0 * q
+        Am[6][6] = 0.66339358229484093853735932809963
+        Am[6][7] = 1.1101113807200520490365790644713*r
+        Am[6][8] = -1.1101113807200520490365790644713*q
         Am[6][9] = 0.0
-        Am[6][10] = -1.0 * w
-        Am[6][11] = v
-
-        # Row 7: v_dot (Sway Acceleration)
+        Am[6][10] = -1.1101113807200520490365790644713*w
+        Am[6][11] = 1.1101113807200520490365790644713*v
         Am[7][0:3] = 0.0
-        Am[7][3] = -0.07194244604316546762589928057554 * cos_pitch * cos_roll * (41092.031908954495559091375453296 * radius ** 3 - 234.459)
-        Am[7][4] = 0.07194244604316546762589928057554 * sin_pitch * sin_roll * (41092.031908954495559091375453296 * radius ** 3 - 234.459)
+        Am[7][3] = -0.5542994585596420683408982805902*cos_pitch*cos_roll
+        Am[7][4] = 0.5542994585596420683408982805902*sin_pitch*sin_roll
         Am[7][5] = 0.0
-        Am[7][6] = -1.0 * r
-        Am[7][7] = self.damping_sign * 2.005755395683453237410071942446 # Linear Drag (Y)
+        Am[7][6] = -0.90081051087988085109735042490748*r
+        Am[7][7] = 0.7501513957886908822554363410929
         Am[7][8] = p
         Am[7][9] = w
         Am[7][10] = 0.0
-        Am[7][11] = -1.0 * u
-
-        # Row 8: w_dot (Heave Acceleration)
+        Am[7][11] = -0.90081051087988085109735042490748*u
         Am[8][0:3] = 0.0
-        Am[8][3] = 0.07194244604316546762589928057554 * cos_pitch * sin_roll * (41092.031908954495559091375453296 * radius ** 3 - 234.459)
-        Am[8][4] = 0.07194244604316546762589928057554 * cos_roll * sin_pitch * (41092.031908954495559091375453296 * radius ** 3 - 234.459)
+        Am[8][3] = 0.5542994585596420683408982805902*cos_pitch*sin_roll
+        Am[8][4] = 0.5542994585596420683408982805902*cos_roll*sin_pitch
         Am[8][5] = 0.0
-        Am[8][6] = q
-        Am[8][7] = -1.0 * p
-        Am[8][8] = self.damping_sign * 3.0316546762589928057553956834532 # Linear Drag (Z)
-        Am[8][9] = -1.0 * v
-        Am[8][10] = u
+        Am[8][6] = 0.90081051087988085109735042490748*q
+        Am[8][7] = -1.0*p
+        Am[8][8] = 1.1338371527451733779857994050809
+        Am[8][9] = -1.0*v
+        Am[8][10] = 0.90081051087988085109735042490748*u
         Am[8][11] = 0.0
+        Am[9][0:3] = 0.0
+        Am[9][3] = 22.064013840830449826989619377163*cos_pitch*cos_roll
+        Am[9][4] = -22.064013840830449826989619377163*sin_pitch*sin_roll
+        Am[9][5:9] = 0.0
+        Am[9][9] = 2.802768166089965397923875432526
+        Am[9][10] = -0.43503277217070903803294482236905*r
+        Am[9][11] = -0.43503277217070903803294482236905*q
+        Am[10][0:4] = 0.0
+        Am[10][4] = 19.169501127127782529271437106573*cos_pitch
+        Am[10][5] = 0.0
+        Am[10][6] = 5.5412526536013647028260744793525*w
+        Am[10][7] = 0.0
+        Am[10][8] = 5.5412526536013647028260744793525*u
+        Am[10][9] = 0.50914915170048566293331160158391*r
+        Am[10][10] = 2.435081300552576468079646209727
+        Am[10][11] = 0.50914915170048566293331160158391*p
+        Am[11][0:6] = 0.0
+        Am[11][6] = -4.0213389143211517422715271307161*v
+        Am[11][7] = -4.0213389143211517422715271307161*u
+        Am[11][8] = 0.0
+        Am[11][9] = -0.095203664338187202006697551579755*q
+        Am[11][10] = -0.095203664338187202006697551579755*p
+        Am[11][11] = 1.7671612910636088319231574039434
 
-        # Row 9: p_dot (Roll Angular Acceleration)
-        Am[9][0:9] = 0.0
-        Am[9][9] = self.damping_sign * 4.8191481416942570511065196285878 # Rotational Drag (Roll)
-        Am[9][10] = -0.50519031141868512110726643598616 * r
-        Am[9][11] = -0.50519031141868512110726643598616 * q
-
-        # Row 10: q_dot (Pitch Angular Acceleration)
-        Am[10][0:9] = 0.0
-        Am[10][9] = 0.55658914728682170542635658914729 * r
-        Am[10][10] = self.damping_sign * 4.3185544587585745357202610005019 # Rotational Drag (Pitch)
-        Am[10][11] = 0.55658914728682170542635658914729 * p
-
-        # Row 11: r_dot (Yaw Angular Acceleration)
-        Am[11][0:9] = 0.0
-        Am[11][9] = -0.071504802561366061899679829242263 * q
-        Am[11][10] = -0.071504802561366061899679829242263 * p
-        Am[11][11] = self.damping_sign * 2.9727509347911212118885467933018 # Rotational Drag (Yaw)
 
         return Am
 
