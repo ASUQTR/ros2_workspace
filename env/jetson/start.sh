243ff72e202d891764e5953ac8371856194bd1cb
PietroLacommande <pietrolacommande@outlook.com>
2026-07-15 02:31:53 -0400

    Updated docker installation method

--- env/jetson/start.sh
+++ env/jetson/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ENV_FILE="$(pwd)/env/jetson"
-CONTAINER_NAME="${ASUQTR_CONTAINER_NAME:-asuqtr_ros2_manual_assisted}"
+CONTAINER_NAME="${ASUQTR_CONTAINER_NAME:-asuqtr_ros2}"
 
 if ! docker images --format json | grep -q "asuqtr_ros2"; then
   echo -e "[\033[0;31m ERROR \033[0m] Missing docker image.."
@@ -11,8 +11,10 @@ docker run -it --rm \
   --privileged \
   --group-add dialout \
   --net=host \
+  --shm-size=1gb \
   -v /dev:/dev \
   -v $(pwd)/workspace:/workspace \
+  -v /tmp/argus_socket:/tmp/argus_socket \
   -v /mnt/nvme:/mnt/nvme \
   --name "$CONTAINER_NAME" \
   asuqtr_ros2
