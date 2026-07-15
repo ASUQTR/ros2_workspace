af53923187ded125def1e2209ac0b40a1a42211b
PietroLacommande <pietrolacommande@outlook.com>
2026-07-15 02:58:25 -0400

    updated based on new docker name

--- connect-jetson.sh
+++ connect-jetson.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-CONTAINER_NAME="${ASUQTR_CONTAINER_NAME:-asuqtr_ros2_manual_assisted}"
+CONTAINER_NAME="${ASUQTR_CONTAINER_NAME:-asuqtr_ros2}"
 
 docker exec -it "$CONTAINER_NAME" /bin/bash
